070fd51e79fe8cb0597e4922a65c285830f3835d
hangCode <1981616118@qq.com>
2024-10-19 23:53:57 +0800

    chore: pluginView

--- packages/pluginView/src/App.tsx
+++ packages/pluginView/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with Preact</h1>
+      <h1>分包加载代码以来分析</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
